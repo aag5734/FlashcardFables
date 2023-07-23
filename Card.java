40d2719474265839b7fe412c3e2ddbfc2084fd88
aag5734 <abrahamgonzalez3764@gmail.com>
2023-07-23 15:57:47 -0700

    Initial commit

---
+++ Card.java
@@ -0,0 +1,12 @@
+public class Card {
+    private String term, def;
+    private int seen = 0;
+    public Card(String word, String definiton) {
+        term = word;
+        def = definiton;
+    }
+
+    public getTerm() {
+        
+    }
+}
