b824fb258a612ad009faca6e3382cb44c21b4b99
aag5734 <abrahamgonzalez3764@gmail.com>
2023-07-30 23:12:55 -0700

    add: basic card implementaion

--- Card.java
+++ Card.java
@@ -2,32 +2,80 @@ public class Card {
     private String term, def;
     private int familiarity = 0;
 
+    /**
+     * Constructor for the Card class.
+     * Creates a new Card object with the specified term and definition.
+     *
+     * @param word The term or word on the card.
+     * @param definition The definition of the term on the card.
+     */
     public Card(String word, String definiton) {
         term = word;
         def = definiton;
     }
 
+    /**
+     * Get the term or word on the card.
+     *
+     * @return The term on the card.
+     */
     public String getTerm() {
         return term;
     }
 
+    /**
+     * Get the definition of the term on the card.
+     *
+     * @return The definition of the term on the card.
+     */
     public String getDef() {
         return def;
     }
 
+    /**
+     * Get the familiarity level of the card.
+     * The familiarity level indicates how well the user knows the term.
+     * It increases with repeated correct responses and decreases with incorrect responses.
+     *
+     * @return The familiarity level of the card.
+     */
     public int getFamiliarity() {
         return familiarity;
     }
 
+    /**
+     * Increment the familiarity level of the card.
+     * This method is called when the user correctly matches a term to its definition.
+     */
     public void incFamiliarity() {
         familiarity++;
     }
 
+    /**
+     * Decrement the familiarity level of the card.
+     * This method is called when the user incorrectly matches a term to its definition.
+     */
     public void decFamiliarity() {
         familiarity--;
     }
 
+    /**
+     * Check if the card is mastered.
+     * A card is considered mastered when its familiarity level is equal to or greater than 5.
+     *
+     * @return true if the card is mastered, false otherwise.
+     */
     public boolean isMastered() {
         return familiarity >= 5;
+    }
+
+    /**
+     * Override the toString method to provide a custom string representation of the Card object.
+     *
+     * @return A string representation of the card in the format: "term: definition".
+     */
+    @Override
+    public String toString() {
+        return this.term + ": " + this.def + "\n";
     }
 }
--- Player.java
+++ Player.java
@@ -1,8 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Player {
     ArrayList<Card> cards;
+
+    /**
+     * Constructor for the Player class.
+     * Prompts the user to enter the path of a text file containing card data,
+     * and then initializes the cards ArrayList by calling makeCards method.
+     */
     public Player () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter txt file path: ");
+        cards = makeCards(scanner.nextLine());
+    }
+
+    /**
+     * Reads card data from a given text file and creates Card objects.
+     *
+     * @param filename The path of the text file containing card data.
+     * @return An ArrayList of Card objects created from the data in the text file.
+     */
+    public ArrayList<Card> makeCards(String filename) {
         cards = new ArrayList<>();
+        try {
+            BufferedReader cardCSV = new BufferedReader(new FileReader(filename));
+            String line = cardCSV.readLine();
+            while (line != null) {
+                String[] card_args = line.split("\t");
+                this.cards.add(new Card(card_args[0], card_args[1]));
+                line = cardCSV.readLine();
+            }
+            cardCSV.close();
+        } catch (IOException e) {
+            System.err.println("Path not found.");
+            return this.cards;
+        }
+        return this.cards;
+    }
+
+    /**
+     * Prints the details of all the cards in the player's cards ArrayList.
+     */
+    public void printCards() {
+        for (Card c : this.cards) {
+            System.out.println(c.toString());
+        }
     }
 }
